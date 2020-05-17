8a43afd10a55bdcf0c589b486da5711705284372
Satas <s.kapoius.11@gmail.com>
2020-05-17 13:17:31 +0100

    fix

--- flaskr/init.py
+++ flaskr/init.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, abort, jsonify
 
-from models import set_up, Movie
-
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
