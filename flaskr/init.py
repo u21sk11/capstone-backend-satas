e46b56fc774ee62d11dc5f59f576bb78c6314202
Satas <s.kapoius.11@gmail.com>
2020-05-17 17:57:23 +0100

    cors

--- flaskr/init.py
+++ flaskr/init.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, abort, jsonify
-from flask_cors import CORS
 
 from models import Movie, Actor
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    cors = CORS(app)
     
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization, true')
         response.headers.add('Access-Control-Allow-Methods', 'PATCH,POST,DELETE,OPTIONS')
         return response
     
@@ -20,11 +18,14 @@ def create_app(test_config=None):
 
     @app.route('/actors', methods=['GET'])
     def crud_actors():
-        actors = Actor.query.all()
         actors_formatted = []
-        for a in actors:
-            actors_formatted.append(a.format)
-        
+        try:
+            actors = Actor.query.all()
+            for a in actors:
+                actors_formatted.append(a.format)
+        except ConnectionError:
+            abort(503)
+
         return jsonify(actors_formatted)
 
     return app
--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
 
 from flaskr.init import create_app
 from models import db, set_up
 
 app = create_app()
+cors = CORS(app)
 set_up(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
